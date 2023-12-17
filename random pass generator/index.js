54a3a14dc8d30cdd4d765c6bc75a779cbb73277d
suryapeddireddy <suryapeddireddy1203@gmail.com>
2023-12-17 23:45:37 +0530

    few changes were made

--- random pass generator/index.js
+++ random pass generator/index.js
@@ -10,23 +10,20 @@ document.querySelector('.btn').addEventListener('click', () => {
   const helloElement = document.querySelector('.hello');
 
   // Check if 'box' class is present before removing it
-  if (helloElement.classList.contains('box')) {
-    helloElement.classList.remove('box');
-  }
+
 
   // Update the content after a short delay (adjust the delay as needed)
   update();
-
-  // Add the 'box' class back after a short delay
-  setTimeout(() => {
-    // Show the element after adding the 'box' class
     helloElement.style.display = 'block';
 
     helloElement.classList.add('box');
-    helloElement.textContent = `text copied!`;
-  }, 100); // 100 milliseconds delay (adjust as needed)
+    helloElement.textContent = `password generated!`;
 
-  ans = "";
+    ans = "";
+    setTimeout(() => {
+      helloElement.textContent="";
+      helloElement.classList.remove('box');  
+    }, 1000);
 });
 
 function update() {
